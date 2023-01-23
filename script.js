8566a19b4feaa87b16224c95adae33349280b780
Levin <molebarth07@gmail.com>
2023-01-23 16:08:58 +0100

    erster Punkt

---
+++ script.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('canvas-id');
+var context = canvas.getContext('2d');
+
+var cellLength = 16;
+context.fillStyle = 'green';
+context.fillRect(0, 0, cellLength, cellLength)
