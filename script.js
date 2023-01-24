7603edf93e6257b927cb664b91cf4a555a6d05b9
Levin <molebarth07@gmail.com>
2023-01-24 12:56:24 +0100

    Bewegung

--- script.js
+++ script.js
@@ -4,10 +4,26 @@ var context = canvas.getContext('2d');
 
 var cellLength = 16;
 
+//Startposition der Schlange
+var snake = {
+    x:0,
+    y:0
+}
+
 function drawCell(x, y, color) {
     context.fillStyle = color;
     context.fillRect(x*cellLength, y*cellLength, cellLength-1, cellLength-1)
 }
 
-drawCell(10, 10, 'green')
-drawCell(11, 10, 'green')
+// Funktion zum Bewegen der Schlange
+function moveSnake (){
+    window.requestAnimationFrame(moveSnake) 
+    drawCell(snake.x,snake.y,'black')
+    snake.x=snake.x+1
+    if(snake.x>24){
+        snake.x=0
+    }
+    drawCell(snake.x, snake.y,'green')
+
+}
+moveSnake()
