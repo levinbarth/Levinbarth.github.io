5f0d484f4413294959a771e2aacc99e1eda7cb82
Levin <molebarth07@gmail.com>
2023-01-24 10:55:40 +0100

    Wurm/Funktion

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+'use-strict';
 var canvas = document.getElementById('canvas-id');
 var context = canvas.getContext('2d');
 
 var cellLength = 16;
-context.fillStyle = 'green';
-context.fillRect(0, 0, cellLength, cellLength)
+
+function drawCell(x, y, color) {
+    context.fillStyle = color;
+    context.fillRect(x*cellLength, y*cellLength, cellLength-1, cellLength-1)
+}
+
+drawCell(10, 10, 'green')
+drawCell(11, 10, 'green')
