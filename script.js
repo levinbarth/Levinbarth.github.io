2f701c9246cb6322aa49cbf01666317c3e3a91bd
Levin <molebarth07@gmail.com>
2023-01-25 12:58:57 +0100

    Selber auffressen

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ var cellLength = 16;
 
 //Startposition der Schlange
 var speedCounter
-var snake = {
+var snake
+var newDirection
+
+function resetGame(){
+  context.clearRect(0,0,canvas.width,canvas.height)
+  speedCounter = 0
+  snake = {
     cells: [
         {x:7,y:0},
         {x:6,y:0},
@@ -19,8 +25,10 @@ var snake = {
     ],
     length: 8,
     direction: 'r'
-
+  }
+  newDirection = 'r'
 }
+
 
 function drawCell(x, y, color) {
     context.fillStyle = color;
@@ -46,14 +54,14 @@ function getNewX(){
 }
 
 function getNewY(){
-  if(snake.direction === 'u'){
+  if(snake.direction === 'd'){
     var newY = snake.cells[0].y + 1
     if(newY === 25){
         newY=0
     }
     return newY
   }  
-  if(snake.direction === 'd'){
+  if(snake.direction === 'u'){
     var newY = snake.cells[0].y - 1
     if(newY === -1){
         newY=24
@@ -61,12 +69,18 @@ function getNewY(){
     return newY
   }
   return snake.cells[0].y
-function getNewY(){
-    return 0
 }
+function doesSnakeContain(x, y){
+  var snakeContains = false
+  snake.cells.forEach(function (cell){
+    if(x===cell.x && y===cell.y){
+      snakeContains = true
+    }
+  })
+ return snakeContains
 }
+
 // Funktion zum Bewegen der Schlange
-speedCounter=0
 function moveSnake (){
     window.requestAnimationFrame(moveSnake)
     speedCounter=speedCounter+1
@@ -77,15 +91,14 @@ function moveSnake (){
 
 
     drawCell(snake.cells[snake.length-1].x, snake.cells[snake.length-1].y ,'black')
-    
-    var newX = snake.cells[0].x + 1
-    if(newX === 25){
-        newX=0
-    }
-
-
+  
+    snake.direction = newDirection;
     var newX = getNewX()
     var newY = getNewY()
+    if(doesSnakeContain(newX,newY)){
+      resetGame()
+      return
+    }
 
     snake.cells.unshift({x: newX, y: newY})
     snake.cells.pop()
@@ -97,29 +110,32 @@ document.addEventListener('keydown', function(input) {
     switch(input.key) {
         case  'ArrowLeft':
           if(snake.direction !== 'r') {
-            snake.direction = 'l';
+            newDirection = 'l';
           }
           break;
     
         case  'ArrowRight':
-            if(snake.direction !== 'l') {
-              snake.direction = 'r';
+          if(snake.direction !== 'l') {
+            newDirection = 'r';
+          }
+          break;
+        
+        case  'ArrowUp':
+            if(snake.direction !== 'd') {
+              newDirection = 'u';
             }
             break;
         
-        case  'ArrowUp':
-                if(snake.direction !== 'd') {
-                  snake.direction = 'u';
-                }
-                break;
-        
          case  'ArrowDown':
-                     if(snake.direction !== 'u') {
-                      snake.direction = 'd';
-                    }
-                    break;
+            if(snake.direction !== 'u') {
+              newDirection = 'd';
+            }
+            break;
         default:
           // code block
       }
 })
+
+// start game
+resetGame()
 moveSnake()
